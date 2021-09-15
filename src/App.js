5ef8cc2c9393ce585fe3c63308905ca0d5256e14
Moises Brandao <moisesbrandao1998@gmail.com>
2021-09-15 06:51:37 -0300

    Created basic composition of routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact to='/'>
 
-    </>
+        </Route>
+
+        <Route exact to='/cadastro'>
+
+        </Route>
+
+        <>
+          {/* NAVBAR */}
+          {/* SIDEBAR */}
+          <Route exact to='/timeline'>
+
+          </Route>
+
+          <Route exact to='/myposts'>
+
+          </Route>
+
+          <Route exact to='/mylikes'>
+
+          </Route>
+
+          <Route exact to='/user/:id'>
+
+          </Route>
+
+          <Route exact to='/hashtag/:hashtag'>
+
+          </Route>
+          <>
+          </Switch>
+        </BrowserRouter>
   );
 }
