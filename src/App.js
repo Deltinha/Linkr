6d9c2b58e4f59927e7fbb3cab7f0c335f09bcfe3
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-09-15 19:14:51 -0300

    Removed unused useHistory import from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import * as S from './AppStyled'
 import './reset.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { useHistory } from 'react-router';
 import SignUp from './screens/SignUp/SignUp';
 import LogIn from './screens/LogIn/LogIn';
 import { UserContext } from './contexts/UserContext';
