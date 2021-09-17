f7f821eb5bfeced94c46cce1191e4e1cf45c61ae
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-09-17 00:57:22 -0300

    Toggleable behavior added to navbar menu

--- src/components/shared/Navbar/index.js
+++ src/components/shared/Navbar/index.js
@@ -1,17 +1,69 @@
-import { useEffect } from "react";
+import { useEffect, Component } from "react";
 import { useHistory } from "react-router";
 import { FaAngleDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useState } from "react/cjs/react.development";
 import { getUserInfo } from "../../../services/linkr-api";
 import * as S from './style';
+import enhanceWithClickOutside from "react-click-outside";
+
 
 export default function Navbar (){
     
     let history = useHistory();
     const [userAvatar, setUserAvatar] = useState('');
     const userID = localStorage.getItem('userID');
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    function toggleShowMenu(input) {
+        if (input){
+            setMenuOpened(true);
+            console.log('mostrei');
+        }
+        else {
+            setMenuOpened(false);
+            console.log('escondi');
+        }
+    }
+
+    const style = {
+        backgroundColor: '#green',
+        border: '2px solid #f0f',
+      };
+
+  const MenuHandler = (() => {
+    class MenuHandler extends Component {
+        handleClickOutside() {
+        toggleShowMenu(false);
+      }
+
+      render() {
+        if (menuOpened){
+            return (
+                <div style={style}>
+                    <S.NavbarMenu onClick={()=>toggleShowMenu(false)}>
+                        <FaAngleDown />
+                        <img src={`${userAvatar}`} alt='User Avatar'/>
+                    </S.NavbarMenu>
+                    <S.NavbarLinks>
+                                <Link to='/myposts'>My posts</Link>
+                                <Link to='/mylikes'>My likes</Link>
+                                <span onClick={()=>logout()}>Logout</span>
+                    </S.NavbarLinks>
+                </div>
+                );
+        }
+        return (
+            <S.NavbarMenu onClick={()=>toggleShowMenu(true)}>
+                        <FaAngleDown />
+                        <img src={`${userAvatar}`} alt='User Avatar'/>
+            </S.NavbarMenu>
+        );
+      }
+    }
+    return enhanceWithClickOutside(MenuHandler);
+  })();
 
     function logout(){
         localStorage.clear();
@@ -34,17 +86,7 @@ export default function Navbar (){
         return (
             <S.Navbar>
                 <span>linkr</span>
-    
-                <S.NavbarMenu className='click-outside-listener'>
-                    <FaAngleDown />
-                    
-                    <img src={`${userAvatar}`} alt='User Avatar'/>
-                </S.NavbarMenu>
-                <S.NavbarLinks>
-                    <Link to='/myposts'>My posts</Link>
-                    <Link to='/mylikes'>My likes</Link>
-                    <span onClick={()=>logout()}>Logout</span>
-                </S.NavbarLinks>
+                <MenuHandler />
             </S.Navbar>
         );
 }
