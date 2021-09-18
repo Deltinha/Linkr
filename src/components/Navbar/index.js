0c02e13b73e5ba4ce616abc344a931e5cbf5a7a0
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-09-18 16:03:36 -0300

    Added link to timeline on linkr woodmark

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -71,7 +71,7 @@ export default function Navbar (){
 
         return (
             <S.Navbar>
-                <span>linkr</span>
+                <span onClick={()=>goToRoute('/timeline')}>linkr</span>
                 <MenuHandler />
             </S.Navbar>
         );
