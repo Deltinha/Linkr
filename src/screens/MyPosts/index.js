7fd20227e372363634941e3362f690a74933156f
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-09-20 16:02:38 -0300

    Fixed wrong alert and posts not loading after deletion on myLikes page

--- src/screens/MyPosts/index.js
+++ src/screens/MyPosts/index.js
@@ -48,7 +48,7 @@ export default function MyPosts() {
 							<WarningMessage>Nenhum Post Encontrado</WarningMessage>
 						) : (
 							posts.map((post) => (
-								<Post key={post.id} data={post} poster={post.user} likes={post.likes} />
+								<Post fetchPosts={fetchPosts} key={post.id} data={post} poster={post.user} likes={post.likes} />
 							))
 						)}
 					</PostsContainer>
