fb293f9dfbdd87f0793f1f674e0e32c7f34935da
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-09-24 13:14:44 -0300

    Devounce time and gotoUserPage

--- src/components/Navbar/SearchUser/index.js
+++ src/components/Navbar/SearchUser/index.js
@@ -1,11 +1,19 @@
-import { SearchUserWrapper } from "./style";
+import { SearchUserWrapper, SuggestionsList } from "./style";
 import { useState } from "react";
 import { getSearchUser } from "../../../services/linkr-api";
+import { DebounceInput } from "react-debounce-input";
+import { useHistory } from "react-router";
 
 export default function SearchUser(){
     const token = localStorage.getItem('token');
     const [value, setValue] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+    const history = useHistory();
+
+    function visitUserPage(userID){
+        setValue('');
+        history.push(`/user/${userID}`)
+    }
 
     function updateSuggestions(e) {
         
@@ -13,21 +21,31 @@ export default function SearchUser(){
         console.log(value);
         
 
-        if (e.target.value.length > 2) {
+        if (e.target.value.length < 3) {
+            setSuggestions([]);
+        }
+        else {
             getSearchUser({token , queryString:e.target.value})
                 .then((res)=>setSuggestions(res.data.users))
-                .catch((err)=>console.log(err.response))
+                .catch((err)=>console.log(err.response));
         }
 
     };
 
     return (
         <SearchUserWrapper>
-            <input
-            type='text'
-            value={value}
+            <DebounceInput 
+            minLength={3}
+            debounceTimeout={300}
             onChange={e => updateSuggestions(e)}/>
 
+            <SuggestionsList>
+                {suggestions.map((suggestion)=>(
+                    <li onClick={()=>visitUserPage(suggestion.id)}>
+                        <span>{suggestion.username}</span>
+                    </li>
+                ))}
+            </SuggestionsList>
         </SearchUserWrapper>
     );
 }
--- src/components/Navbar/SearchUser/style.js
+++ src/components/Navbar/SearchUser/style.js
@@ -1,8 +1,34 @@
 import styled from "styled-components";
 
 const SearchUserWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
+    input {
+        position: relative;
+        width: 563px;
+        height: 45px;
+        border-radius: 8px;
+        border: none;
+        background-color: white;
+        padding-left: 17px;
+        padding-right: 17px;
+    }
+
+    input:focus {
+        outline: none;
+    }
 `;
 
-export {SearchUserWrapper};
+const SuggestionsList = styled.ul`
+    background-color: #E7E7E7;
+    position: absolute;
+    width: 563px;
+
+    li {
+        color: #515151;
+        font-family: 'Lato';
+        font-size: 19px;
+    }
+`;
+
+export {SearchUserWrapper,
+        SuggestionsList,
+};
