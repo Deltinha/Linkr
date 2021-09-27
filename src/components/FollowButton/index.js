d29d5c6e9ef2e8f01aa8b405f626b5fbf6ca475f
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2021-09-27 16:58:50 -0300

    fix: follow button collors

--- src/components/FollowButton/index.js
+++ src/components/FollowButton/index.js
@@ -1,4 +1,3 @@
-
 import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router";
 import UserContext from "../../contexts/UserContext";
@@ -6,64 +5,63 @@ import { postFollowUser, postUnfollowUser } from "../../services/linkr-api";
 import { FollowButtonWrapper } from "./style";
 import Loader from "react-loader-spinner";
 
-export default function FollowButton(){
-    const token = localStorage.getItem("token");
-    const { id } = useParams();
-    const {updateFollowedUsers} = useContext(UserContext);
-	const {followedUsers} = useContext(UserContext);
+export default function FollowButton() {
+	const token = localStorage.getItem("token");
+	const { id } = useParams();
+	const { updateFollowedUsers } = useContext(UserContext);
+	const { followedUsers } = useContext(UserContext);
 	const followedUsersString = JSON.stringify(followedUsers);
-    const [followingUser, setFollowingUser] = useState(false);
-    const [disabledButton, setDisabledButton] = useState(false);
+	const [followingUser, setFollowingUser] = useState(false);
+	const [disabledButton, setDisabledButton] = useState(false);
 
-	function followUser (){
-        setDisabledButton(true);
-		postFollowUser({token, userID: id})
-			.then((res)=>{
-                setDisabledButton(false);
-                setFollowingUser(true);
-            })
-			.catch((err)=>{
-                setDisabledButton(false);
-                alert('não foi possível seguir o usuário')
-            })
+	function followUser() {
+		setDisabledButton(true);
+		postFollowUser({ token, userID: id })
+			.then((res) => {
+				setDisabledButton(false);
+				setFollowingUser(true);
+			})
+			.catch((err) => {
+				setDisabledButton(false);
+				alert("não foi possível seguir o usuário");
+			});
 	}
 
-    function unfollowUser (){
-        setDisabledButton(true);
-		postUnfollowUser({token, userID: id})
-			.then((res)=>{
-                setDisabledButton(false);
-                setFollowingUser(false);
-            })
-			.catch((err)=>{
-                setDisabledButton(false);
-                alert('não foi possível deixar de seguir o usuário')
-            })
+	function unfollowUser() {
+		setDisabledButton(true);
+		postUnfollowUser({ token, userID: id })
+			.then((res) => {
+				setDisabledButton(false);
+				setFollowingUser(false);
+			})
+			.catch((err) => {
+				setDisabledButton(false);
+				alert("não foi possível deixar de seguir o usuário");
+			});
 	}
 
-    function verifyIsUserFollowed(){
-		return Boolean(
-			followedUsers.find((user)=>user.id === Number(id))
-		);
+	function verifyIsUserFollowed() {
+		return Boolean(followedUsers.find((user) => user.id === Number(id)));
 	}
 
-    useEffect(()=>{
+	useEffect(() => {
 		updateFollowedUsers();
 		setFollowingUser(verifyIsUserFollowed());
-		
 	}, [token, id, followedUsersString]);
 
-    return (
-        <FollowButtonWrapper 
-        onClick={followingUser ? unfollowUser : followUser}
-        disabled={disabledButton}>
-            {
-                disabledButton ?
-                <Loader type="ThreeDots" color="#FFF" height={10} />
-                :
-                followingUser ? 'Unfollow' : 'Follow'
-            }
-            
-        </FollowButtonWrapper>
-    );
+	return (
+		<FollowButtonWrapper
+			onClick={followingUser ? unfollowUser : followUser}
+			disabled={disabledButton}
+			isFollowing={followingUser}
+		>
+			{disabledButton ? (
+				<Loader type="ThreeDots" color="#FFF" height={10} />
+			) : followingUser ? (
+				"Unfollow"
+			) : (
+				"Follow"
+			)}
+		</FollowButtonWrapper>
+	);
 }
--- src/components/FollowButton/style.js
+++ src/components/FollowButton/style.js
@@ -3,20 +3,20 @@ import styled from "styled-components";
 const FollowButtonWrapper = styled.button`
 	font-family: "Lato", sans-serif;
 	font-size: 14px;
-    font-weight: 700;
+	font-weight: 700;
 	width: 112px;
 	height: 31px;
 	border: none;
 	border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-	
-    cursor: ${({disabled})=>disabled ? 'default' : 'pointer'};
-	background-color: ${({disabled})=>disabled ? 'rgba(24, 119, 242, 0.4)' : 'rgb(24, 119, 242)'};
-	color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	cursor: ${({ disabled }) => (disabled ? "default" : "pointer")};
+	background-color: ${({ disabled }) =>
+		disabled ? "rgba(24, 119, 242, 0.4)" : "rgb(24, 119, 242)"};
+	background-color: ${(props) => (props.isFollowing ? "#FFFFFF" : "#1877F2")};
+	color: ${(props) => (props.isFollowing ? "#1877F2" : "white")};
 `;
 
-export {
-    FollowButtonWrapper,
-}
+export { FollowButtonWrapper };
